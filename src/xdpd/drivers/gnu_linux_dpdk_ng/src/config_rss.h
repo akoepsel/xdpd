4439e5f5efcbf17f5a54ff52e207cd1643fb3745
Andreas Koepsel <andreas.koepsel@bisdn.de>
2017-12-13 14:58:33 +0100

    store ifname in phyports structure

--- src/xdpd/drivers/gnu_linux_dpdk_ng/src/config_rss.h
+++ src/xdpd/drivers/gnu_linux_dpdk_ng/src/config_rss.h
@@ -41,6 +41,8 @@ struct phyport {
 	unsigned int nb_tx_queues; //number of txqueues to be used, (nb_rx_queues == nb_tx_queues)
 	int is_vf; //0:not a virtual function, 1:virtual function
 	int parent_port_id; //-1:no parent port, only valid when is_vf is true
+	int ifindex;
+	char ifname[IF_NAMESIZE+1];
 };
 
 /**
