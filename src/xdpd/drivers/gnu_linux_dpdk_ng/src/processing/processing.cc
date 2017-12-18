caeca314d70970d49cc667ffd8caa15a9f8e0467
Andreas Koepsel <andreas.koepsel@bisdn.de>
2017-12-18 18:07:51 +0100

    sync

--- src/xdpd/drivers/gnu_linux_dpdk_ng/src/processing/processing.cc
+++ src/xdpd/drivers/gnu_linux_dpdk_ng/src/processing/processing.cc
@@ -919,30 +919,32 @@ int processing_packet_reception(void* not_used){
 				const uint16_t nb_rx = rte_eth_rx_burst(port_id, queue_id, mbufs, PROC_ETH_RX_BURST_SIZE);
 
 				if (nb_rx) {
-					RTE_LOG(INFO, USER1, "%u packets received from port %u, queue %u\n", nb_rx, port_id, queue_id);
-				}
+					RTE_LOG(INFO, USER1, "RX task => %u packets received from eth-port %u, eth-queue %u\n", nb_rx, port_id, queue_id);
 
-				for (i = 0; i < nb_rx; i++) {
-					event[i].flow_id = mbufs[i]->hash.rss;
-					event[i].op = RTE_EVENT_OP_NEW;
-					event[i].sched_type = RTE_SCHED_TYPE_ATOMIC;
-					event[i].queue_id = ev_queue_id;
-					event[i].event_type = RTE_EVENT_TYPE_ETHDEV;
-					event[i].sub_event_type = 0;
-					event[i].priority = RTE_EVENT_DEV_PRIORITY_NORMAL;
-					event[i].mbuf = mbufs[i];
-				}
-
-				const int nb_tx = rte_event_enqueue_burst(eventdev_id, ev_port_id, event, nb_rx);
-				if (nb_tx) {
-					RTE_LOG(INFO, USER1, "RX task => %u events enqueued on ev-queue %u via ev-port %u\n", nb_tx, ev_queue_id, ev_port_id);
-				}
-				/* release mbufs not queued in event device */
-				if (nb_tx != nb_rx) {
-					for(i = nb_tx; i < nb_rx; i++) {
-						RTE_LOG(WARNING, USER1, "RX task %u: dropping mbuf[%u] on port %u, queue %u\n", lcore_id, i, ev_port_id, ev_queue_id);
-						rte_pktmbuf_free(mbufs[i]);
+					for (i = 0; i < nb_rx; i++) {
+						event[i].flow_id = mbufs[i]->hash.rss;
+						event[i].op = RTE_EVENT_OP_NEW;
+						event[i].sched_type = RTE_SCHED_TYPE_ATOMIC;
+						event[i].queue_id = ev_queue_id;
+						event[i].event_type = RTE_EVENT_TYPE_ETHDEV;
+						event[i].sub_event_type = 0;
+						event[i].priority = RTE_EVENT_DEV_PRIORITY_NORMAL;
+						event[i].mbuf = mbufs[i];
 					}
+
+					const int nb_tx = rte_event_enqueue_burst(eventdev_id, ev_port_id, event, nb_rx);
+					if (nb_tx) {
+						RTE_LOG(INFO, USER1, "RX task => %u events enqueued on ev-queue %u via ev-port %u\n", nb_tx, ev_queue_id, ev_port_id);
+					}
+					/* release mbufs not queued in event device */
+					if (nb_tx != nb_rx) {
+						for(i = nb_tx; i < nb_rx; i++) {
+							RTE_LOG(WARNING, USER1, "RX task %u: dropping mbuf[%u] on port %u, queue %u\n", lcore_id, i, ev_port_id, ev_queue_id);
+							rte_pktmbuf_free(mbufs[i]);
+						}
+					}
+				} else {
+					rte_pause();
 				}
 			}
 			rte_rwlock_read_unlock(&port_list_rwlock);
