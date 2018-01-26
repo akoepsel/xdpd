3cf79e03079bfc1a73d820efaa8002421827a3d4
Andreas Koepsel <andreas.koepsel@bisdn.de>
2018-01-26 12:11:50 +0100

    sync

--- src/xdpd/drivers/gnu_linux_dpdk_ng/src/io/tx.h
+++ src/xdpd/drivers/gnu_linux_dpdk_ng/src/io/tx.h
@@ -80,7 +80,7 @@ tx_pkt(switch_port_t* port, unsigned int queue_id, datapacket_t* pkt){
 		int i = 0, nb_rx = 1;
 		const int nb_tx = rte_event_enqueue_burst(eventdev_id, ev_port_id, tx_events, nb_rx);
 
-		if (lcores[lcore_id].is_master){
+		if (lcore_id != LCORE_ID_ANY && lcores[lcore_id].is_master){
 			RTE_LOG(DEBUG, XDPD, "wk-task-%02u: on socket MASTER, enqueued %u events\n",
 					lcore_id, nb_tx);
 		}
