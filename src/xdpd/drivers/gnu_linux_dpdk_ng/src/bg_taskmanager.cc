d92c46c3937a380e6b94386d42dd4a8b764fe661
Andreas Koepsel <andreas.koepsel@bisdn.de>
2018-02-07 17:13:12 +0100

    sync

--- src/xdpd/drivers/gnu_linux_dpdk_ng/src/bg_taskmanager.cc
+++ src/xdpd/drivers/gnu_linux_dpdk_ng/src/bg_taskmanager.cc
@@ -134,7 +134,7 @@ int process_timeouts(){
 void* x86_background_tasks_routine(void* param){
 
 	static struct timeval last_time_stats_updated={0,0}, last_time_links_updated={0,0}, now;
-#if 1
+#if 0
 	static struct timeval last_time_task_stats_updated={0,0};
 #endif
 #ifdef GNU_LINUX_DPDK_ENABLE_NF
@@ -169,7 +169,7 @@ void* x86_background_tasks_routine(void* param){
 		}
 #endif
 
-#if 1
+#if 0
 		//Update port stats
 		if(get_time_difference_ms(&now, &last_time_task_stats_updated)>=BG_UPDATE_TASK_STATS_MS){
 			processing_update_stats();
