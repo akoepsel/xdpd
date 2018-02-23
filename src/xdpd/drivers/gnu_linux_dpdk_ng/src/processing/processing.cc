696b8f5f93c5252cdc7023bbcf731ac427c40f8c
Andreas Koepsel <andreas.koepsel@bisdn.de>
2018-02-23 16:27:11 +0100

    sync

--- src/xdpd/drivers/gnu_linux_dpdk_ng/src/processing/processing.cc
+++ src/xdpd/drivers/gnu_linux_dpdk_ng/src/processing/processing.cc
@@ -994,10 +994,10 @@ int processing_packet_pipeline_processing_v2(void* not_used){
 				task->stats.tx_pkts+=nb_tx;
 
 				/* statistics */
-				if (unlikely((task->stats.rx_pkts & uint64_t(0x00000000ffffffff)) == 0)) {
+				//if (unlikely((task->stats.rx_pkts & uint64_t(0x00000000ffffffff)) == 0)) {
 					RTE_LOG(INFO, XDPD, "wk-task-%u.%02u => rcvd %" PRIu64" pkts, sent %" PRIu64" pkts, dropped %" PRIu64" pkts, threshold: %" PRIu64"\n",
-							socket_id, lcore_id, task->stats.rx_pkts, task->stats.tx_pkts, task->stats.pkts_dropped, task->stats.rx_pkts % uint64_t(1<<20));
-				}
+							socket_id, lcore_id, task->stats.rx_pkts, task->stats.tx_pkts, task->stats.pkts_dropped, (task->stats.rx_pkts & uint64_t(0x00000000ffffffff)));
+				//}
 			}
 
 #if 0
